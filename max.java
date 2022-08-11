8cb263353a3ab26b119411d7b9355f5856c5de41
Cesarion7 <90016241+Cesarion7@users.noreply.github.com>
2022-08-11 10:53:47 -0500

    Update max.java
    Added method to calculate the max number in an array of foats

--- max.java
+++ max.java
@@ -1,7 +1,7 @@
 
 class max{
  
-    public static int Intgr(int[] i){
+    public static int Int(int[] i){
         int a=i[0];
         for (int j=1; j<i.length;j++){
             if (a<i[j]){
@@ -9,5 +9,15 @@ class max{
             }
         }
         return a;    
+    }
+
+    public static float Float(float[] f){
+         float a=f[0];
+         for(int j=0;j<f.length;j++){
+            if(a<f[j]){
+               a=f[j];
+            }
+         }
+         return a;
     }
 }
